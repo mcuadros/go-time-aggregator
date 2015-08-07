2f9025d38606ceec4a2cd037b563bbecc23481f7
Carlos Cobo <toqueteos@gmail.com>
2015-08-07 12:14:18 +0200

    Add GobEncode and GobDecode methods for gob support.

--- encoder.go
+++ encoder.go
@@ -1,6 +1,8 @@
 package aggregator
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 
 	"labix.org/v2/mgo/bson"
@@ -42,4 +44,88 @@ func (a *TimeAggregator) MarshalJSON() ([]byte, error) {
 	}
 
 	return json.Marshal(v)
+}
+
+func (a *TimeAggregator) GobEncode() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+
+	var err error
+	err = enc.Encode(a.kind)
+	if err != nil {
+		return nil, err
+	}
+
+	err = enc.Encode(a.flags)
+	if err != nil {
+		return nil, err
+	}
+
+	err = enc.Encode(uint32(len(a.Values)))
+	if err != nil {
+		return nil, err
+	}
+
+	for p, a := range a.Values {
+		err = enc.Encode(p)
+		if err != nil {
+			return nil, err
+		}
+		err = enc.Encode(a.values)
+		if err != nil {
+			return nil, err
+		}
+		err = enc.Encode(a.p)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (a *TimeAggregator) GobDecode(body []byte) error {
+	buf := bytes.NewReader(body)
+	dec := gob.NewDecoder(buf)
+
+	var err error
+	err = dec.Decode(&a.kind)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&a.flags)
+	if err != nil {
+		return err
+	}
+
+	a.Values = make(map[Period]*aggregator)
+
+	var length uint32
+	err = dec.Decode(&length)
+	if err != nil {
+		return err
+	}
+
+	for i := uint32(0); i < length; i++ {
+		var p Period
+		err = dec.Decode(&p)
+		if err != nil {
+			return err
+		}
+
+		var agg aggregator
+		err = dec.Decode(&agg.values)
+		if err != nil {
+			return err
+		}
+		err = dec.Decode(&agg.p)
+		if err != nil {
+			return err
+		}
+
+		a.Values[p] = &agg
+	}
+
+	return nil
 }
